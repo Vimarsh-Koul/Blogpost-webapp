59f762666498f01b1e968e484835bcd4402d3830
vimarsh-koul <vimarsh2001koul@gmail.com>
2020-11-08 01:35:00 +0530

    Added functionality for creating,updating and deleting posts

--- cs/blog/urls.py
+++ cs/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from .views import postlistview,postdetailview,postcreateview,postupdateview,postdeleteview
 
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('about/',views.about,name='about')
+    path('',postlistview.as_view(), name='home'),
+    path('post/<int:pk>/',postdetailview.as_view(), name='post-detail'),
+    path('post/new/',postcreateview.as_view(), name='post-create'),
+    path('about/',views.about,name='about'),
+    path('post/<int:pk>/update',postupdateview.as_view(), name='post-update'),
+    path('post/<int:pk>/delete',postdeleteview.as_view(), name='post-delete'),
 ]
--- cs/blog/views.py
+++ cs/blog/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import post
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 
 # Create your views here.
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2018'
+#     }
+# ]
 
 def home(request):
     context = {
@@ -27,3 +29,47 @@ def home(request):
 
 def about(request):
     return render(request,'blog/about.html')    
+
+
+class postlistview(ListView):
+    model = post
+    template_name = 'blog/home.html'
+    context_object_name='posts'
+    ordering = ['-date_posted']
+
+class postdetailview(DetailView):
+    model = post
+
+class postcreateview(LoginRequiredMixin,CreateView):
+    model = post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
+    
+
+class postupdateview(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class postdeleteview(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = post
+    success_url='/'
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
